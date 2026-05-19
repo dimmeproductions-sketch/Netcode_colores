36358f364dad5011c157411653656925d4a6dd1c
Daniel Martinez Cobas <dimmeproductions@gmail.com>
2026-05-20 00:02:46 +0200

    Version 1.0 - Codigo funcionando y pulido generado a partir del apartado "Getting Started" de NetCode for GameObjects de la documentación de Unity. El jugador cambia de color tanto si se le pide a través del cliente como si se pide desde el propio servidor.

--- Assets/Scripts/CodePlayer.cs
+++ Assets/Scripts/CodePlayer.cs
@@ -5,37 +5,41 @@ namespace HelloWorld
 {
     public class HelloWorldPlayer : NetworkBehaviour
     {
-        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+        public NetworkVariable<int> ColorIndex = new NetworkVariable<int>();
+        private readonly Color[] presetColors = new Color[] { Color.red, Color.blue, Color.green, Color.yellow, Color.magenta, Color.cyan };
+        private Renderer m_Renderer;
+
+        private void Awake()
+        {
+            m_Renderer = GetComponent<Renderer>();
+        }
 
         public override void OnNetworkSpawn()
         {
             if (IsOwner)
             {
-                Move();
+                ChangeColor();
             }
         }
 
-        public void Move()
+        public void ChangeColor()
         {
-            SubmitPositionRequestRpc();
+            SubmitColorRequestRpc();
         }
 
         [Rpc(SendTo.Server)]
-        private void SubmitPositionRequestRpc(RpcParams rpcParams = default)
+        private void SubmitColorRequestRpc(RpcParams rpcParams = default)
         {
-            var randomPosition = GetRandomPositionOnPlane();
-            transform.position = randomPosition;
-            Position.Value = randomPosition;
-        }
-
-        static Vector3 GetRandomPositionOnPlane()
-        {
-            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+            int randomColorIndex = Random.Range(0, presetColors.Length);
+            ColorIndex.Value = randomColorIndex;
         }
 
         private void Update()
         {
-            transform.position = Position.Value;
+            if (m_Renderer != null && ColorIndex.Value >= 0 && ColorIndex.Value < presetColors.Length)
+            {
+                m_Renderer.material.color = presetColors[ColorIndex.Value];
+            }
         }
     }
 }
